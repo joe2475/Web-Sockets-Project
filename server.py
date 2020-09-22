48d36950d895eab6c957bfd3355deaf5b0d9a583
joe2475 <33675219+joe2475@users.noreply.github.com>
2020-09-22 12:22:54 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,11 +3,12 @@ import socket
 #AF_INET is what corrosponds with IPV4 and SOCK_STREAM is for TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
-#Que of 5 listens in case traffic becomes to intense
+#Que of 5 listens in case traffic becomes full
 s.listen(5)
 
 while True:
     print("WEB PROXY SERVER IS LISTENING")
     clientSocket, address = s.accept()
+
     #Sends data to the client socket
     clientSocket.send()
