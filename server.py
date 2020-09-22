ca1aae80a4f0dc30f944e8066703998c493f5b8b
Joseph Barsis <33675219+joe2475@users.noreply.github.com>
2020-09-22 12:15:53 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,13 @@
+import socket 
+
+#AF_INET is what corrosponds with IPV4 and SOCK_STREAM is for TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))
+#Que of 5 listens in case traffic becomes to intense
+s.listen(5)
+
+while True:
+    print("WEB PROXY SERVER IS LISTENING")
+    clientSocket, address = s.accept()
+    #Sends data to the client socket
+    clientSocket.send()
