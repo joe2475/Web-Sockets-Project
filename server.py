64158346916ce70328bd08863479d457008201e8
joe2475 <33675219+joe2475@users.noreply.github.com>
2020-10-29 20:05:41 -0500

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ host = 'localhost'
 #AF_INET is what corrosponds with IPV4 and SOCK_STREAM is for TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Host name will be localhost and 1234 is the port number. Port will change just used it for testing purposes. 
-s.bind((host, 1234))
+s.bind((host, 50000))
 #Que of 5 listens in case traffic becomes full
 s.listen(5)
 #print(socket.gethostname())
