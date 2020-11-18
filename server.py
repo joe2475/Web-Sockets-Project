7f606d423bc729c7e670318035807e776bbcfdec
imposterFish <51309759+imposterFish@users.noreply.github.com>
2020-11-17 22:16:23 -0600

    Combined server.py and server2.py
    NOT TESTED
    DO NOT MERGE

--- server.py
+++ server.py
@@ -1,16 +1,23 @@
 import socket 
 import socketserver
+import ssl
+import os
 
-#####################
+###############################
 # Cache is a list of lists
 # The first element of the inner list is the URL and the second is the data
-#####################
+# TODO: Remove
+###############################
 cache = []
 def cacheObject(destAddr, data): # - No.7 MS
     # v parsed before (or in the function - change params)
     temp = [destAddr, data]
     cache.append(temp)
+##############################################################
 
+###############################
+# Proxy server set up
+###############################
 host = 'localhost'
 #AF_INET is what corresponds with IPV4 and SOCK_STREAM is for TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,7 +26,79 @@ s.bind((host, 50000))
 #Que of 5 listens in case traffic becomes full
 s.listen(5)
 #print(socket.gethostname())
+##############################################################
 
+###############################
+# getWebpage
+# Connects to webpage, gets webdata and stores in current directory. 
+# Will overwrite files
+# TODO: Clean up, error handling, 
+# TODO: GET images and ect.
+# TODO: Possibly handle other requests other than GET
+###############################
+def getWebpage(hostName): #hostName is the address of the server
+    # Creates SSL context with recommended security settings. Includes cert. verification
+    context = ssl.create_default_context()
+
+    # Connect to a server
+    hostname='www.google.com'
+    conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=hostname)
+    conn.connect((hostname, 443))
+
+    request=bytes("GET / HTTP/1.1\nHost: % s\r\n\r\n" % (hostname), 'utf-8')
+    #request=bytes("HEAD / HTTP/1.1\nHost: % s\r\n\r\n" % (hostname), 'utf-8')
+
+    conn.sendall(request)
+
+    try:
+        if os.path.exists(hostname+".webdoc"):
+            os.remove(hostname+".webdoc")
+        f = open(hostname+".webdoc",'a')
+        conn.settimeout(1)#idk 100 seemed like a good number
+        while True:
+            data = conn.recv(1000)#.split(b"\r\n")
+            if data:
+                print(data) 
+                f.write(data.decode('utf-8'))
+            else:
+                break
+        conn.close()
+        f.close()
+        
+    except:
+        conn.close()
+        print('hi')
+        f.close()
+
+    # This gets rid of the header
+    try:
+        if os.path.exists(hostname+".html"):
+            os.remove(hostname+".html")
+        f = open(hostname+".webdoc",'r')
+        f2 = open(hostname+".html",'a')
+        webline = f.readline()
+        while webline:
+            if webline.upper().find('<!DOCTYPE HTML>')>-1:
+                break
+            webline = f.readline()
+        print (webline)
+        while webline:
+            f2.write(webline)
+            webline = f.readline() 
+        f.close()
+        f2.close()
+        os.remove(hostname+".webdoc")
+    except:
+        print('file error')
+        f.close()
+        f2.close()
+    
+    return
+##############################################################
+
+###############################
+# Running Proxy
+###############################
 while True:
     print("WEB PROXY SERVER IS LISTENING")
     clientSocket, address = s.accept()
@@ -52,7 +131,9 @@ while True:
         break # TODO: Request webpage from webserver, receive webdata, parse and add to cache. Also remove break
         # Then send data to client. This is a big part
 
+    getWebpage(destAddr)
     exit()
+##############################################################
     # clientSocket.close()
 
 
