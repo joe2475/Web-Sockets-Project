88f66b5654e6acf9506418862d781846be6442c5
joe2475 <33675219+joe2475@users.noreply.github.com>
2020-11-22 11:45:35 -0600

    Update server.py
    Can display webpage and parse headers sent by the end server.

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket 
-import socketserver
-
+#import socketserver
+import urllib.request
+import ssl
 host = 'localhost'
 #AF_INET is what corrosponds with IPV4 and SOCK_STREAM is for TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,15 +10,17 @@ s.bind((host, 50000))
 #Que of 5 listens in case traffic becomes full
 s.listen(5)
 #print(socket.gethostname())
+s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 while True:
     print("WEB PROXY SERVER IS LISTENING")
     clientSocket, address = s.accept()
-    data = clientSocket.recv(10000)
+    inData = clientSocket.recv(10000)
     #print(data)
     print("MESSAGE RECEIVED FROM CLIENT:")
     #Parses and decodes the incoming http request 
-    data = data.decode('utf-8')
+    data = inData.decode('utf-8')
+   # print("This is the data " + data)
     getRequest = data.split('\n')[0]
     method = getRequest.split(' ')[0]
     destAddr = (getRequest.split(' ')[1])[1:]
@@ -28,7 +31,31 @@ while True:
     print("END OF MESSAGE RECEIVED FROM CLIENT")
     print("[PARSE MESSAGE HEADER]")
     print(f'METHOD = {method}, DESTADDRESS = {destAddr}, HTTPVersion = {version}')
-    
+    print("Dest:" + destAddr)
+    #Set up to send the data to the desired destination
+    dest = 'Host: ' + destAddr + ':80\r\n'
+    server_address = (destAddr, 80)
+    destination = ('Host: ' + destAddr + ':80\r\n')
+    message  = b'GET / HTTP/1.1\r\n'
+    message += bytes(dest, encoding='utf-8')
+    message += b'Connection: close\r\n'
+    message += b'\r\n'
+    #Uses a second socket to connect to the destination server 
+    s2.connect(server_address)
+    s2.sendall(message)
+    #Parses response header
+    respdata = b''
+    while True:
+        buf = s2.recv(1024)
+        if not buf:
+            break
+        respdata += buf
+    temp = respdata.decode('utf-8')
+    print(temp)
+    s2.close()
+    clientSocket.sendall(respdata)
+    clientSocket.close()
     exit()
-   # clientSocket.close()
- #   s.close()
+     #  conn.send(sentData)
+    #clientSocket.close()
+    #s.close()
