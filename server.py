3f2c87ee045424e12f7b7b0b33d365bbfd16ec36
joe2475 <33675219+joe2475@users.noreply.github.com>
2020-11-22 14:26:54 -0600

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,6 @@ s.bind((host, 50000))
 s.listen(5)
 #Second socket that is used to connect to the web server. 
 s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 while True:
     print("WEB PROXY SERVER IS LISTENING")
     clientSocket, address = s.accept()
@@ -21,19 +20,19 @@ while True:
     print("MESSAGE RECEIVED FROM CLIENT:")
     #Parses and decodes the incoming http request 
     data = inData.decode('utf-8')
-   # print("This is the data " + data)
     getRequest = data.split('\n')[0]
     method = getRequest.split(' ')[0]
     destAddr = (getRequest.split(' ')[1])[1:]
     version = getRequest.split(' ')[2]
     data = data.split('\n')
+    message2 = ''
     for x in range(len(data)):
         print(data[x])
     print("END OF MESSAGE RECEIVED FROM CLIENT")
     print("[PARSE MESSAGE HEADER]")
     print(f'METHOD = {method}, DESTADDRESS = {destAddr}, HTTPVersion = {version}')
     print("Dest:" + destAddr)
-    #Set up to send the data to the desired destination
+    print("REQUEST MESSAGE SENT TO ORIGINAL SERVER:")
     dest = 'Host: ' + destAddr + ':80\r\n'
     server_address = (destAddr, 80)
     destination = ('Host: ' + destAddr + ':80\r\n')
@@ -41,22 +40,29 @@ while True:
     message += bytes(dest, encoding='utf-8')
     message += b'Connection: close\r\n'
     message += b'\r\n'
-    #Uses a second socket to connect to the destination server 
+    print("END OF MESSAGE SENT TO ORIGINAL SERVER")
+    #Uses a second socket to connect to the destination server
     s2.connect(server_address)
     s2.sendall(message)
     #Parses response header
     respdata = b''
-    while True:
+    while True: 
         buf = s2.recv(1024)
         if not buf:
             break
         respdata += buf
-    temp = respdata.decode('utf-8')
-    print(temp)
+    response = respdata.decode('utf-8', 'ignore')
+    #Prints the reponse
+    print("REPSONSE HEADER FROM ORIGINAL SERVER")
+    #Range is 10 as to not print the html data
+    for x in range(10):
+        print(response.split('\n')[x])
+    print("END OF HEADER")
+   # print(temp)
     s2.close()
     clientSocket.sendall(respdata)
     clientSocket.close()
-    exit()
-     #  conn.send(sentData)
-    #clientSocket.close()
+    #exit()
     #s.close()
+    break
+s.close()
